c9574a42422537ce69b43919e90f07e53599dd2e
Odi <ezurikeodinaka@gmail.com>
2026-01-11 01:11:32 +0100

    Initial commit: add EasyScripts VS Code extension (v0.1 alpha) with syntax highlighting and themes

---
+++ easyscript-vscode/easyscript/out/extension.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+function activate() {
+    console.log("EasyScript activated");
+}
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
---
+++ easyscript-vscode/easyscript/src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from "vscode";
+
+export function activate() {
+  console.log("EasyScript activated");
+}
+
+export function deactivate() {}
